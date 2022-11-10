512f6786f02dff4329e6a754322fc829995079e8
Charlie <charlienikki999@gmail.com>
2022-11-10 23:15:32 +0800

    睡觉咯~

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+        <School />
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    // 汇总所有的组件
+    import School from './components/School.vue'
 
     export default {
         name: 'App',
+        // 组件标签
         components: {
-            HelloWorld,
+            School,
         },
     }
 </script>
-
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
---
+++ src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="school">
+        <h1 v-text="msg"></h1>
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <button @click="showInfo()">点我会有好事发生</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {
+            return {
+                msg: '欢迎学习Vue',
+                name: '中国地质大学(武汉)',
+                address: '湖北武汉市洪山区',
+            }
+        },
+        methods: {
+            showInfo() {
+                console.log('1111')
+            }
+        },
+    }
+</script>
+
+<style>
+    .school {
+        background-color: aqua;
+    }
+</style>
