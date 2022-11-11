b1da8aa640641739efdd8a584109a30636b18fa7
Charlie <charlienikki999@gmail.com>
2022-11-11 15:10:44 +0800

    11

---
+++ src/components/Student.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="school">
+        <h1 v-text="msg"></h1>
+        <h2>学生姓名：{{ name }}</h2>
+        <h2>学生年龄：{{ age }}</h2>
+        <button @click="showName()">点我提示名称</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {
+            return {
+                msg: '欢迎学习Vue',
+                name: '张三',
+                age: '18',
+            }
+        },
+        methods: {
+            showName() {
+                console.log(this. 
+                name)
+            },
+        },
+    }
+</script>
+
+<style>
+    .school {
+        background-color: aqua;
+    }
+</style>
